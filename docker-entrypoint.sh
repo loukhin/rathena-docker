1cf0ea7b55674c1320be565cd658212cc0b5c49c
LoukHin <loukhin3141@gmail.com>
2021-06-26 04:30:21 +0700

    ✏️ fix - remove duplicate chown

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,6 @@ _main() {
     echo "Updating config files..."
     cp -r /opt/rathena/custom-conf/* /opt/rathena/conf
 
-    chown -R rathena /opt/rathena
-
     setpriv --reuid=rathena --regid=rathena --init-groups "$@"
 }
 
