85d236b60a77b16501271250be883af2d0e41815
LoukHin <loukhin3141@gmail.com>
2021-06-26 00:06:53 +0700

    ✏️ fix - wrong variable name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 _main() {
     files=( "conf" "db" "npc" "athena-start" "char-server" "login-server" "map-server" "function.sh" )
-    for file in "${$files[@]}"
+    for file in "${files[@]}"
     do
         cp -rn /opt/src/rathena/$file /opt/rathena
     done
