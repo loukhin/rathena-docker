4d794c3ce739028bec3ab960c79a413430730c95
LoukHin <loukhin3141@gmail.com>
2021-06-28 07:50:39 +0700

    ✏️ fix - load npc files only in map server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,15 @@ set -e
 _main() {
     echo "Updating config files..."
     cp -r /opt/rathena/custom-conf/* /opt/rathena/conf
-    echo "Updating npc files..."
-    cp -r /opt/rathena/custom-npc/* /opt/rathena/npc
+
+    if [ $@ == "map" ]; then
+        echo "Updating npc files..."
+        cp -r /opt/rathena/custom-npc/* /opt/rathena/npc
+    fi
 
     chown -R rathena:rathena /opt/rathena
 
-    setpriv --reuid=rathena --regid=rathena --init-groups "$@"
+    setpriv --reuid=rathena --regid=rathena --init-groups "/opt/rathena/$@-server"
 }
 
 _main "$@"
