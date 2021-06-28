363da8c00d02bb0d4791bc3d8a82a8de037be746
LoukHin <loukhin3141@gmail.com>
2021-06-28 07:46:01 +0700

    ✨ npc - add custom npc loader

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 _main() {
     echo "Updating config files..."
     cp -r /opt/rathena/custom-conf/* /opt/rathena/conf
+    echo "Updating npc files..."
+    cp -r /opt/rathena/custom-npc/* /opt/rathena/npc
 
     chown -R rathena:rathena /opt/rathena
 
